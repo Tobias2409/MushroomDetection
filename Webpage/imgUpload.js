b65fd36d7388684d307f2b355666e2074d0be55b
Tobias2409 <to.schiffelhumer@gmail.com>
2022-02-10 23:08:15 +0100

    Improve Webpage

--- Webpage/imgUpload.js
+++ Webpage/imgUpload.js
@@ -3,5 +3,6 @@ let $ = document.querySelector.bind(document)
 
 $("#upload").onchange = () => {
     $("#imageResult").src = URL.createObjectURL($("#upload").files[0]);
+    $("#imageResultContainer").style.visibility = "visible";
     $("#upload-label").innerHTML = $("#upload").files[0].name;
 };
