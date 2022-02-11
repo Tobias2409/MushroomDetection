8eb5d5a65c8c9315a0968b53feda8e95d1322f76
Tobias2409 <to.schiffelhumer@gmail.com>
2022-02-11 10:40:48 +0100

    Analyze Mushroom

--- Python/MushroomAi.py
+++ Python/MushroomAi.py
@@ -4,4 +4,24 @@ def getLabelFromFile(name):
   return name.split("_")[0]
 
 learner = load_learner("MushroomAi.pkl", cpu=True, pickle_module=pickle)
-print(learner.predict(input()))
+
+import socket
+
+HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    while True:
+      conn, addr = s.accept()
+      with conn:
+          print('Connected by', addr)
+          while True:
+              data = conn.recv(1024).decode()
+              print("../" + data)
+              if not data:
+                  break
+              print(learner.predict("../" + data)[0]);
+              conn.sendall(bytes("" + learner.predict("../" + data)[0], "utf-8"))
+
--- Webpage/analyze.php
+++ Webpage/analyze.php
@@ -51,3 +51,16 @@ if ($uploadOk == 0) {
 }
 ?>
 
+<?php
+    $host = "127.0.0.1";
+    $port = 65432;
+    // No Timeout
+    set_time_limit(0);
+
+    $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket\n");
+    $result = socket_connect($socket, $host, $port) or die("Could not connect to server\n");
+    socket_write($socket, $target_file, strlen($target_file)) or die("Could not send data to server\n");
+    $result = socket_read ($socket, 1024) or die("Could not read server response\n");
+    echo "Reply From Server  :".$result;
+    socket_close($socket);
+?>
